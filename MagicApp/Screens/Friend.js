17f5de6bde43e43207002db8f48151963f6d30e1
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-01-15 00:01:03 -0500

    Fix modal for detail

--- MagicApp/Screens/Friend.js
+++ MagicApp/Screens/Friend.js
@@ -12,86 +12,90 @@ import React from "react"
 
 export default class Friend extends React.Component {
 
-	constructor(props) {
-		super(props)
-	}
+    constructor(props) {
+        super(props)
+    }
 
-	componentDidMount() {
+    componentDidMount() {
 
-	}
+    }
 
-	render() {
+    render() {
 
-		return <View
-				style={styles.friend}>
-				<View
-					style={{
-						flexDirection: "row",
-					}}>
-					<Image
-						source={require("./../assets/images/avatar.png")}
-						style={styles.avatarImage}/>
-					<View>
-						<Text
-							style={styles.jeremyHarrisonText}>Jeremy Harrison{"\
-"}</Text>
-						<View
-							style={{
-								flex: 1,
-								justifyContent: "flex-end",
-							}}>
-							<Text
-								style={styles.iLikeBasketballAnText}>I like basketball and my name is blablbalbalbalblablab and dasdadasds fsfsd.{"\
-"}</Text>
-						</View>
-					</View>
-				</View>
-				<View
-					style={{
-						flex: 1,
-						justifyContent: "flex-end",
-					}}/>
-			</View>
-	}
+        return <View
+        style={styles.friend}>
+        <View
+        style={{
+        flexDirection: "row",
+        }}>
+        <Image
+        source={require("./../assets/images/avatar.png")}
+        style={styles.avatarImage}/>
+        <View>
+        <Text
+        style={styles.jeremyHarrisonText}>Jeremy Harrison</Text>
+        <View
+        style={{
+        flex: 1,
+       justifyContent: "center",
+        }}>
+        <Text
+        style={styles.iLikeBasketballAnText}>I like basketball and my name is blablbalbalbalblablab and dasdadasds fsfsd.</Text>
+        </View>
+        </View>
+        </View>
+        <View
+        style={{
+        }}/>
+        </View>
+    }
 }
 
 const styles = StyleSheet.create({
-	friend: {
-		backgroundColor: 'rgba(0, 0, 0, 0.0)',
-		height: 89,
-	},
-	avatarImage: {
-		resizeMode: "center",
-		backgroundColor: 'rgba(0, 0, 0, 0.0)',
-		marginLeft: 16,
-		width: 73,
-		height: 74,
-	},
-	jeremyHarrisonText: {
-		color: 'rgb(68, 68, 68)',
-		fontSize: 20,
-		fontStyle: "normal",
-		fontWeight: "bold",
-		textAlign: "center",
-		letterSpacing: -0.8,
-		backgroundColor: 'rgba(0, 0, 0, 0.0)',
-		marginLeft: 5,
-		marginRight: 16,
-		alignSelf: "stretch",
-		height: 27,
-	},
-	iLikeBasketballAnText: {
-		color: 'rgb(68, 68, 68)',
-		fontSize: 14,
-		fontStyle: "normal",
-		fontWeight: "normal",
-		textAlign: "left",
-		letterSpacing: -0.56,
-		backgroundColor: 'rgba(0, 0, 0, 0.0)',
-		marginLeft: 5,
-		marginRight: 16,
-		marginBottom: 22,
-		alignSelf: "stretch",
-		height: 38,
-	},
-})
+                                 friend: {
+                                 backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                                 height: 89,
+                                 },
+
+                                 avatarImage: {
+                                 resizeMode: "center",
+                                 backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                                 //marginLeft: 16,
+                                 width: 74,
+                                 height: 74,
+                                 marginLeft: 10,
+                                 marginRight: 10,
+                                 },
+
+                                 jeremyHarrisonText: {
+                                 color: 'rgb(68, 68, 68)',
+                                 fontSize: 20,
+                                 fontStyle: "normal",
+                                 fontWeight: "bold",
+                                 textAlign: "left",
+                               //  letterSpacing: -0.8,
+                                 //backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                                  // marginLeft: 0,
+                                //  marginRight: 0,
+                               //   alignSelf: "flex-start",
+                                 height: 27,
+                                 },
+
+                                 iLikeBasketballAnText: {
+                                 color: 'rgb(68, 68, 68)',
+                                 fontSize: 14,
+                                 fontStyle: "normal",
+                                 fontWeight: "normal",
+                                 textAlign: "left",
+                                 //letterSpacing: -0.56,
+                                 backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                                 paddingRight: 130,
+                                 flex: 1,
+                                 // justifyContent: "flex-end",
+                                //flexGrow: 1,
+                                // flex: 0.8,
+                                 //alignSelf: "flex-end",
+                                 height: 38,
+                                 },
+
+                                 })
