41fba8c435e3adc1b1e90f743e67cb1a68fa6aad
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-01-17 23:04:44 -0500

    make ios indepedent & add icon for android & rename android

--- MagicApp/ios/magic/AppDelegate.m
+++ MagicApp/ios/magic/AppDelegate.m
@@ -18,12 +18,15 @@
   [FIRApp configure];
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"magic"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
+  
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
