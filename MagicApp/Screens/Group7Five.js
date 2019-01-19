c97c924053f804e6129e0ec58082ab207c04b244
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-01-18 22:18:41 -0500

    export android apk and work on exporting ios ipa

--- MagicApp/Screens/Group7Five.js
+++ MagicApp/Screens/Group7Five.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, View,  Dimensions, Platform, PixelRatio, TouchableHighlight, Modal } from "react-native"
+import { Text, StyleSheet, View,  Dimensions, Platform, PixelRatio, TouchableHighlight, Modal, ScrollView } from "react-native"
 import React from "react"
 import Menu from './Menu'
 
@@ -172,8 +172,8 @@ export default class Group7Five extends React.Component {
     render() {
 
 
-
-        return <View
+      if (Platform.OS == 'android') {
+        return (<View
 
         opacity={this.state.disabled ? 0.5 : 1}style={styles.group7}>
 
@@ -246,12 +246,14 @@ export default class Group7Five extends React.Component {
         }}>
 
 
-
-        <Text numberOfLines={2}
-
+        <View style={{height: 27}} onStartShouldSetResponder={() => {console.log("scroll bro"); this.props.setScroll(false); return true;}} onResponderRelease={() => {console.log("done leave me"); this.props.setScroll(true);}} >
+        <ScrollView onMomentumScrollBegin={() => {console.log("started!"); }} onMomentumScrollEnd={() => {console.log("activate now");  this.props.setScroll(true);}}>
+        <Text
         adjustsFontSizeToFit={true}
 
         style={styles.tdInformationSessiText}>{this.props.item.group_name}</Text>
+        </ScrollView>
+        </View>
 
         <View
 
@@ -381,7 +383,220 @@ export default class Group7Five extends React.Component {
 
         </View>
             </TouchableHighlight>
+        </View>);
+      }
+      else if (Platform.OS == 'ios') {
+        return(
+        <View
+
+        opacity={this.state.disabled ? 0.5 : 1}style={styles.group7}>
+
+
+        <TouchableHighlight onPress={() => {this.props.setScroll(true); this.openInfo();}}  disabled={this.state.disabled} underlayColor="white" >
+        <View
+
+        style={styles.group6View}>
+
+        <View
+
+        style={styles.bitmapView}/>
+
+        <View
+
+        style={{
+
+        flex: 1,
+
+        justifyContent: "flex-end",
+
+        }}>
+
+        <View
+
+        style={{
+
+        flexDirection: "row",
+
+        }}>
+
+        <Text
+
+        style={styles.federationHallText}>{this.props.item.location_name}</Text>
+
+        <View
+
+        style={{
+
+        width: "100%",
+
+        height: "100%",
+
+        flexDirection: "row",
+
+        position: "absolute",
+
+        }}>
+
+        <Text
+
+        style={styles.pmText}>{tConvert(this.props.item.time)}</Text>
+
         </View>
+
+        </View>
+
+        </View>
+
+        <View
+
+        style={{
+
+        width: "100%",
+
+        height: "100%",
+
+        position: "absolute",
+
+        }}>
+
+
+
+        <Text numberOfLines={2}
+        adjustsFontSizeToFit={true}
+        style={styles.tdInformationSessiTextIos}>{this.props.item.group_name}</Text>
+
+
+        <View
+
+        style={{
+
+        flexDirection: "row",
+
+        justifyContent: "center",
+
+        }}>
+
+        <View
+
+        style={styles.viewView}/>
+
+        <View
+
+        style={{
+
+          width: 87,
+          height: 87,
+          alignSelf: 'flex-end',
+          justifyContent: "center",
+          position: "absolute",
+          flexDirection: "row",
+
+          // width: "80%",
+          // height: "80%",
+          // flexDirection: "column",
+          // justifyContent: "space-between",
+          // alignItems: "center",
+          // display: "flex",
+          // position: "absolute",
+
+       // width: 80,
+       //   height: 87,
+       //
+       // justifyContent: "center",
+       // // margin: 0 auto,
+       // // alignSelf: "stretch",
+       // alignItems: "center",
+       // // flexDirection: "column-reverse",
+       //
+       // position: "absolute",
+
+        }}>
+
+        <Text
+
+        style={styles.textText}>{"\n"}{this.props.item.number_going} {"\n"}</Text>
+
+        </View>
+
+
+
+        <View
+
+        style={{
+          width: 87,
+          height: 87,
+          alignSelf: 'flex-start',
+          justifyContent: "center",
+          position: "absolute",
+        }}>
+
+        <Text
+
+        style={styles.goingText}>Going</Text>
+
+        </View>
+
+        </View>
+
+        <View
+
+        style={{
+
+        flexDirection: "column",
+
+        }}>
+
+
+
+        <View
+
+        style={{
+
+        flex: 1,
+
+        flexDirection: "row",
+
+        justifyContent: "flex-end",
+
+        }}>
+
+        {this.props.item.free_food &&
+
+            <Text
+
+            style={styles.freeText}>Free Food</Text>
+
+        }
+
+        </View>
+
+
+
+        <View
+
+        style={{
+
+        width: "100%",
+
+        height: "100%",
+
+        flexDirection: "row",
+
+        justifyContent: "center",
+
+        position: "absolute",
+
+        }}/>
+
+        </View>
+
+        </View>
+
+        </View>
+            </TouchableHighlight>
+        </View>);
+
+      }
 
     }
 
@@ -495,7 +710,35 @@ const styles = StyleSheet.create({
 
                                   alignSelf: "stretch",
 
-                                 height: 27,
+
+
+
+
+                                 },
+                                 tdInformationSessiTextIos: {
+
+                                 color: 'rgb(74, 78, 82)',
+
+                                 fontStyle: "normal",
+
+                                 fontWeight: "bold",
+
+                                 textAlign: "left",
+
+                                 letterSpacing: 0,
+
+                                 backgroundColor: 'rgba(0, 0, 0, 0.0)',
+
+                                 marginLeft: 10,
+
+                                 marginRight: 10,
+
+                                 marginTop: 3,
+
+                                  alignSelf: "stretch",
+
+                                  height: 27,
+
 
 
 
