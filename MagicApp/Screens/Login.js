16bd786f6f7128d3c293567947cb1a5ac83ed9e6
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-03-04 12:17:35 -0500

    cleaned up login, signup, app, and router js

--- MagicApp/Screens/Login.js
+++ MagicApp/Screens/Login.js
@@ -1,100 +1,127 @@
-import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button, Image, TouchableHighlight, TouchableOpacity } from 'react-native'
-import firebase from 'react-native-firebase'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from "react-native-responsive-screen"
+//
+//  Login.js
+//  magic version 1
+//
+//  Created by dhvani&dhrumil.
+//  Copyright © 2019 magic. All rights reserved.
+//
 
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  TouchableHighlight,
+  ActivityIndicator,
+  TouchableOpacity
+} from "react-native";
+import firebase from "react-native-firebase";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 export default class Login extends React.Component {
-    state = { email: '', password: '', errorMessage: null }
-    
-    handleLogin = () => {
-        const { email, password } = this.state
-        firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .catch(error => this.setState({ errorMessage: error.message }))
+  state = { email: "", password: "", errorMessage: null, loading: false };
+
+  handleLogin = () => {
+    const { email, password } = this.state;
+      this.setState({errorMessage: null});
+    if(email.trim() == "" || password.trim() == "") {
+       this.setState({errorMessage: "Please fill in all fields!"});
+     }
+    else{
+        this.setState({loading: true});
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => this.setState({ errorMessage: error.message, loading: false }));
     }
-    
-    render() {
-        return (
-                <View style={styles.container}>
-                <Image
-                source={require("./../assets/images/sm-sq.jpg")}
-                style={styles.profileImg} />
-                {this.state.errorMessage &&
-                <Text style={{ color: 'red' }}>
-                {this.state.errorMessage}
-                </Text>}
-                <TextInput
-                style={styles.textInput}
-                autoCapitalize="none"
-                placeholder="Email"
-                onChangeText={email => this.setState({ email })}
-                value={this.state.email}
-                />
-                <TextInput
-                secureTextEntry
-                style={styles.textInput}
-                autoCapitalize="none"
-                placeholder="Password"
-                onChangeText={password => this.setState({ password })}
-                value={this.state.password}
-                />
-                <TouchableOpacity onPress={() => this.handleLogin()} style={styles.loginbutton}>
-                <Text style = {styles.logintext}>
-                Login
-                </Text>
-                </TouchableOpacity>
-                
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('SignUp')}>
-                <Text style = {styles.SignUptext}>
-                Don't have an account? Sign Up
-                </Text>
-                </TouchableOpacity>
-                
-              
-                </View>
-                )
-    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          // source={require("./../assets/images/sm-sq.jpg")}
+          style={styles.profileImg}
+        />
+        {this.state.errorMessage && (
+          <Text style={{ color: "red" }}>{this.state.errorMessage}</Text>
+        )}
+        {this.state.loading && <ActivityIndicator size="large" color="#0000ff" />}
+        <TextInput
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="Email"
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="Password"
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+        <TouchableOpacity
+          onPress={() => this.handleLogin()}
+          style={styles.loginbutton}
+        >
+          <Text style={styles.logintext}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("SignUp")}
+        >
+          <Text style={styles.SignUptext}>Don't have an account? Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-                                 container: {
-                                 flex: 1,
-                                 justifyContent: 'center',
-                                 alignItems: 'center',
-                                 backgroundColor: "#1398FD",
-                                 },
-                                 profileImg: {
-                                 marginTop: hp('10%'),
-                                 marginBottom: hp('10%'),
-                                 height: 150,
-                                 width: 150,
-                                 borderRadius: 80,
-                                 },
-                                 loginbutton: {
-                                 marginTop: 25,
-                                 width: 80,
-                                 height: 30,
-                                 backgroundColor: '#E0A918',
-                                 borderRadius: 50,
-                                 },
-                                 logintext: {
-                                 fontSize: 20,
-                                 textAlign: 'center',
-                                 },
-                                 SignUptext: {
-                                 marginTop: 25,
-                                 color: '#F5F5F5',
-                                 fontSize: 12,
-                                 textAlign: 'center',
-                                 },
-                                 textInput: {
-                                 height: 40,
-                                 width: '80%',
-                                 marginTop: 10,
-                                 backgroundColor: '#EFF0F1',
-                                 borderRadius: 40,
-                                 textAlign: 'center',
-                                 }
-                                 })
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1398FD"
+  },
+  profileImg: {
+    marginTop: hp("10%"),
+    marginBottom: hp("10%"),
+    height: 150,
+    width: 150,
+    borderRadius: 80
+  },
+  loginbutton: {
+    marginTop: 25,
+    width: 80,
+    height: 30,
+    backgroundColor: "#E0A918",
+    borderRadius: 50
+  },
+  logintext: {
+    fontSize: 20,
+    textAlign: "center"
+  },
+  SignUptext: {
+    marginTop: 25,
+    color: "#F5F5F5",
+    fontSize: 12,
+    textAlign: "center"
+  },
+  textInput: {
+    height: 40,
+    width: "80%",
+    marginTop: 10,
+    backgroundColor: "#EFF0F1",
+    borderRadius: 40,
+    textAlign: "center"
+  }
+});
--- MagicApp/Screens/SignUp.js
+++ MagicApp/Screens/SignUp.js
@@ -1,99 +1,128 @@
-import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, TouchableHighlight, Image } from 'react-native'
-import firebase from 'react-native-firebase'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from "react-native-responsive-screen"
+//
+//  SignUp.js
+//  magic version 1
+//
+//  Created by dhvani&dhrumil.
+//  Copyright © 2019 magic. All rights reserved.
+//
 
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+  ActivityIndicator,
+  Image
+} from "react-native";
+import firebase from "react-native-firebase";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 export default class SignUp extends React.Component {
-    state = { email: '', password: '', errorMessage: null }
-    
-    handleSignUp = () => {
-        const { email, password } = this.state
-        firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(user => this.props.navigation.navigate('DynamicScreen'))
-        .catch(error => this.setState({ errorMessage: error.message }))
+  state = { email: "", password: "", errorMessage: null, loading: false };
+
+  handleSignUp = () => {
+    const { email, password } = this.state;
+     this.setState({errorMessage: null});
+    if(email.trim() == "" || password.trim() == "") {
+       this.setState({errorMessage: "Please fill in all fields!"});
+     }
+    else{
+      this.setState({loading: true})
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => this.props.navigation.navigate("DynamicScreen"))
+      .catch(error => this.setState({ errorMessage: error.message, loading: false }));
     }
-    
-    render() {
-        return (
-                <View style={styles.container}>
-                <Image
-                source={require("./../assets/images/sm-sq.jpg")}
-                style={styles.profileImg} />
-                {this.state.errorMessage &&
-                <Text style={{ color: 'red' }}>
-                {this.state.errorMessage}
-                </Text>}
-                <TextInput
-                placeholder="Email"
-                autoCapitalize="none"
-                style={styles.textInput}
-                onChangeText={email => this.setState({ email })}
-                value={this.state.email}
-                />
-                <TextInput
-                secureTextEntry
-                placeholder="Password"
-                autoCapitalize="none"
-                style={styles.textInput}
-                onChangeText={password => this.setState({ password })}
-                value={this.state.password}
-                />
-                <TouchableOpacity onPress={() => this.handleLogin()} style={styles.loginbutton}>
-                <Text style = {styles.logintext}>
-                Sign Up
-                </Text>
-                </TouchableOpacity>
-                
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
-                <Text style = {styles.SignUptext}>
-                Already have an account? Login
-                </Text>
-                </TouchableOpacity>
-                </View>
-                )
-    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          // source={require("./../assets/images/sm-sq.jpg")}
+          style={styles.profileImg}
+        />
+        {this.state.errorMessage && (
+          <Text style={{ color: "red" }}>{this.state.errorMessage}</Text>
+        )}
+        {this.state.loading && <ActivityIndicator size="large" color="#0000ff" />}
+        <TextInput
+          placeholder="Email"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+        <TouchableOpacity
+          onPress={() => this.handleSignUp()}
+          style={styles.loginbutton}
+        >
+          <Text style={styles.logintext}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("Login")}
+        >
+          <Text style={styles.SignUptext}>Already have an account? Login</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-                                 container: {
-                                 flex: 1,
-                                 justifyContent: 'center',
-                                 alignItems: 'center',
-                                 backgroundColor: "#1398FD",
-                                 },
-                                 profileImg: {
-                                 marginTop: hp('10%'),
-                                 marginBottom: hp('10%'),
-                                 height: 150,
-                                 width: 150,
-                                 borderRadius: 80,
-                                 },
-                                 loginbutton: {
-                                 marginTop: 25,
-                                 width: 90,
-                                 height: 30,
-                                 backgroundColor: '#E0A918',
-                                 borderRadius: 50,
-                                 },
-                                 logintext: {
-                                 fontSize: 20,
-                                 textAlign: 'center',
-                                 },
-                                 SignUptext: {
-                                 marginTop: 25,
-                                 color: '#F5F5F5',
-                                 fontSize: 12,
-                                 textAlign: 'center',
-                                 },
-                                 textInput: {
-                                 height: 40,
-                                 width: '80%',
-                                 marginTop: 10,
-                                 backgroundColor: '#EFF0F1',
-                                 borderRadius: 40,
-                                 textAlign: 'center',
-                                 }
-                                 })
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1398FD"
+  },
+  profileImg: {
+    marginTop: hp("10%"),
+    marginBottom: hp("10%"),
+    height: 150,
+    width: 150,
+    borderRadius: 80
+  },
+  loginbutton: {
+    marginTop: 25,
+    width: 90,
+    height: 30,
+    backgroundColor: "#E0A918",
+    borderRadius: 50
+  },
+  logintext: {
+    fontSize: 20,
+    textAlign: "center"
+  },
+  SignUptext: {
+    marginTop: 25,
+    color: "#F5F5F5",
+    fontSize: 12,
+    textAlign: "center"
+  },
+  textInput: {
+    height: 40,
+    width: "80%",
+    marginTop: 10,
+    backgroundColor: "#EFF0F1",
+    borderRadius: 40,
+    textAlign: "center"
+  }
+});
