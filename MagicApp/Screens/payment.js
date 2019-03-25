74fc7d415e1cf8bdb3e92bbc2fb807fe4a5e53cf
DhrumilShah <dhrumil.ketul@gmail.com>
2019-03-25 17:10:48 -0400

    stripe integration v1

--- MagicApp/Screens/payment.js
+++ MagicApp/Screens/payment.js
@@ -3,36 +3,61 @@ import { View } from "react-native";
 import stripe from "tipsi-stripe";
 
 stripe.setOptions({
-  publishableKey: "pk_test_ihRia1aLHxyCBjEMl0p7oqNk"
-});
+                  publishableKey: "pk_test_ihRia1aLHxyCBjEMl0p7oqNk"
+                  });
+
+const FIREBASE_FUNCTION = 'https://us-central1-payment-test-1d907.cloudfunctions.net/charge/'; // TODO: PUT YOUR FIREBASE FUNCTIONS URL HERE
+
+const charge_amount = 500;
+const charge_currency = 'cad';
 
 const theme = {
-  primaryBackgroundColor: "##FFFFFF",
-  secondaryBackgroundColor: "#f0f0f0",
-  primaryForegroundColor: "#3e8be1",
-  secondaryForegroundColor: "#76a9e3",
-  accentColor: "#4286f4",
-  errorColor: "#e12020"
+primaryBackgroundColor: "##FFFFFF",
+secondaryBackgroundColor: "#f0f0f0",
+primaryForegroundColor: "#3e8be1",
+secondaryForegroundColor: "#76a9e3",
+accentColor: "#4286f4",
+errorColor: "#e12020"
 };
 
 export default class NewCardPage extends Component {
-  componentDidMount() {
-    const options = {
-      smsAutofillDisabled: true,
-      theme
-    };
-    stripe
-      .paymentRequestWithCardForm(options)
-      .then(response => {
-        this.props.navigation.navigate("CurrentOrder");
-        // Get the token from the response, and send to your server
-      })
-      .catch(error => {
-        // Handle error
-      });
-  }
+    componentDidMount() {
+        
+        const items = [{
+                       label: 'Whisky',
+                       amount: '50.00',
+                       }]
+        
+        const options = {
+            smsAutofillDisabled: true,
+            theme
+        };
+        
+        const token = stripe.paymentRequestWithCardForm(options)
 
-  render() {
-    return <View />;
-  }
+        console.log(token);
+        
+        this.props.navigation.navigate("CurrentOrder");
+        
+        async function charge(token, amount, currency) {
+            const res = await fetch(FIREBASE_FUNCTION, {
+                                    method: 'POST',
+                                    body: JSON.stringify({
+                                                         token,
+                                                         charge: {
+                                                         amount,
+                                                         currency,
+                                                         },
+                                                         }),
+                                    });
+            const data = await res.json();
+            data.body = JSON.parse(data.body);
+            return data;
+        }
+      
+    }
+    
+    render() {
+        return <View />;
+    }
 }
