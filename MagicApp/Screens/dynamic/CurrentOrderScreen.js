2fb01f7ecb49ab2194f6b8625db3e91eb33c091a
DhrumilShah <dhrumil.ketul@gmail.com>
2019-06-02 20:06:50 -0400

    currentorder screen

--- MagicApp/Screens/dynamic/CurrentOrderScreen.js
+++ MagicApp/Screens/dynamic/CurrentOrderScreen.js
@@ -1,4 +1,4 @@
-import {Text, StyleSheet, FlatList, View, TouchableOpacity} from 'react-native';
+import {Text, StyleSheet, FlatList, View, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import {
   widthPercentageToDP as wp,
@@ -54,35 +54,65 @@ export default class CurrentOrderScreen extends React.Component {
     var currentOrder = this.state.currentOrder;
     return (
       <View style={styles.menuView}>
-        <View style={styles.backgroundView}>
-          <View style={styles.graybackgroundView}>
-            <Text style={styles.nextmoneyText}>10%</Text>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'flex-end',
-              }}>
-              <Text style={styles.buyersneededText}>$95/100</Text>
-            </View>
-          </View>
+            <View style={styles.backgroundView}>
             <TouchableOpacity
+            onPress={() => {
+            this.props.navigation.navigate ('DynamicScreen');
+            }}
+            style={styles.icCloseButton}
+            >
+            <Image
+            source={require ('../../assets/images/ic-close.png')}
+            style={styles.buttonButtonImage}
+            />
+            </TouchableOpacity>
+            
+            <TouchableOpacity
+            style={styles.icCartButton}
             onPress={() => {
             this.props.navigation.navigate ('History');
             }}
-            style={styles.buttonButton}>
+            >
             <Text style={styles.buttonButtonText}>History</Text>
             </TouchableOpacity>
-        </View>
-        <View
-          style={{
+            
+            <View
+            style={{
+            //position: "absolute",
             flex: 1,
-          }}
-        >
+            flexDirection: 'row',
+            justifyContent: 'center',
+            }}
+            >
+            <View style={styles.graybackgroundView}>
+            <Text style={styles.nextmoneyText}>10%</Text>
+            <View
+            style={{
+            // width: "100%",
+            // height: "100%",
+            flex: 1,
+            // position: "absolute",
+            justifyContent: 'flex-end',
+            }}
+            >
+            <Text style={styles.buyersneededText}>$95/100</Text>
+            </View>
+            </View>
+            </View>
+            </View>
+            <View
+            style={{
+            // width: "100%",
+            // height: "100%",
+            // position: "absolute",
+            flex: 1,
+            }}
+            >
           <Text style={styles.shawarmaPlusText}>Current Order</Text>
           <View style={styles.viewFlatListViewWrapper}>
             <FlatList
               renderItem={this.renderViewFlatListCell}
-              //data={currentOrder.foods}
+              data={currentOrder.foods}
               keyExtractor={this._keyExtractor}
               style={styles.viewFlatList}
             />
@@ -125,43 +155,103 @@ const styles = StyleSheet.create ({
     backgroundColor: 'rgb(246, 246, 246)',
     flex: 1,
   },
-  backgroundView: {
-    backgroundColor: 'rgba(55, 58, 61, 0.9)',
-    height: 123,
-    alignItems: 'center',
-  },
-  graybackgroundView: {
-    backgroundColor: 'rgba(226, 175, 47, 0.99)',
-    borderRadius: 35,
-    borderWidth: 2,
-    borderColor: 'rgb(246, 246, 246)',
-    borderStyle: 'solid',
-    marginTop: hp ('5%'),
-    justifyContent: 'center',
-    width: 70,
-    height: 70,
-  },
-  nextmoneyText: {
-    color: 'white',
-    fontSize: 25,
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: 'transparent',
-    marginTop: hp ('1.5%'),
-    width: 70,
-    //height: 29.92,
-  },
-  buyersneededText: {
-    color: 'white',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    textAlign: 'center',
-    backgroundColor: 'transparent',
-    marginBottom: hp ('1.5%'),
-    width: 70,
-  },
+                                  backgroundView: {
+                                  backgroundColor: 'rgba(55, 58, 61, 0.9)',
+                                  height: hp ('16.5%'),
+                                  flexDirection: 'row',
+                                  },
+                                  icCloseButton: {
+                                  backgroundColor: 'transparent',
+                                  shadowColor: 'rgba(0, 0, 0, 0.11)',
+                                  shadowRadius: 3,
+                                  shadowOpacity: 1,
+                                  flexDirection: 'row',
+                                  alignItems: 'center',
+                                  justifyContent: 'center',
+                                  padding: 0,
+                                  marginLeft: wp ('5%'),
+                                  marginTop: hp ('7%'),
+                                  width: 50,
+                                  height: 50,
+                                  },
+                                  icCloseButtonText: {
+                                  color: 'black',
+                                  fontSize: 12,
+                                  fontStyle: 'normal',
+                                  fontWeight: 'normal',
+                                  textAlign: 'left',
+                                  },
+                                  icCloseButtonImage: {
+                                  resizeMode: 'contain',
+                                  },
+                                  icCartButton: {
+                                  backgroundColor: 'transparent',
+                                  shadowColor: 'rgba(0, 0, 0, 0.11)',
+                                  shadowRadius: 3,
+                                  shadowOpacity: 1,
+                                  flexDirection: 'row',
+                                  alignItems: 'center',
+                                  justifyContent: 'flex-start',
+                                  marginLeft: wp ('61%'),
+                                  marginTop: hp ('7%'),
+                                  width: 50,
+                                  height: 50,
+                                  },
+                                  icCartButtonText: {
+                                  color: 'black',
+                                  fontSize: 12,
+                                  fontStyle: 'normal',
+                                  fontWeight: 'normal',
+                                  textAlign: 'left',
+                                  },
+                                  icCartButtonImage: {
+                                  resizeMode: 'contain',
+                                  },
+                                  
+                                  buttonButtonImage: {
+                                  resizeMode: 'contain',
+                                  width: 60,
+                                  height: 60,
+                                  },
+                                  buttonButtonImageTwo: {
+                                  resizeMode: 'contain',
+                                  width: 60,
+                                  height: 60,
+                                  },
+                                  
+                                  graybackgroundView: {
+                                  backgroundColor: 'rgba(226, 175, 47, 0.99)',
+                                  borderRadius: 35,
+                                  borderWidth: 2,
+                                  borderColor: 'rgb(246, 246, 246)',
+                                  borderStyle: 'solid',
+                                  marginTop: hp ('5%'),
+                                  marginRight: wp ('95%'),
+                                  position: 'relative',
+                                  width: 70,
+                                  height: 70,
+                                  },
+                                  nextmoneyText: {
+                                  color: 'white',
+                                  fontSize: 25,
+                                  fontStyle: 'normal',
+                                  fontWeight: 'bold',
+                                  textAlign: 'center',
+                                  backgroundColor: 'transparent',
+                                  marginTop: hp ('1.5%'),
+                                  width: 70,
+                                  //height: 29.92,
+                                  },
+                                  buyersneededText: {
+                                  color: 'white',
+                                  fontSize: 12,
+                                  fontStyle: 'normal',
+                                  fontWeight: 'normal',
+                                  textAlign: 'center',
+                                  backgroundColor: 'transparent',
+                                  marginBottom: hp ('1.5%'),
+                                  width: 70,
+                                  },
   shawarmaPlusText: {
     color: 'rgb(246, 246, 246)',
     fontSize: hp ('4%'),
@@ -267,11 +357,6 @@ const styles = StyleSheet.create ({
     marginRight: 1,
     marginBottom: 6,
     width: 362,
-  },
-  buttonButton: {
-    //position: "absolute",
-    marginLeft: wp ('75%'),
-    marginTop: hp ('-8%'),
   },
   buttonButtonText: {
     color: 'white',
