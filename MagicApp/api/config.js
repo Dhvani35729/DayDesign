76712600b3adb652aef40bf37a13ed3a42213748
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-07-08 22:47:43 -0400

    remove db reference

--- MagicApp/api/config.js
+++ MagicApp/api/config.js
@@ -1,6 +1,6 @@
 import firebase from 'react-native-firebase';
 
-var db = firebase.firestore();
-const user = firebase.auth().currentUser;
+// var db = firebase.firestore();
+const user = firebase.auth ().currentUser;
 
-export {db, user};
+export {user};
